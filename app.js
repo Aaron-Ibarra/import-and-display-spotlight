bcb4e35aa4d3d0a49188f821cb05844c4ea94b73
Aaron Ibarra <aaronibarra132@gmail.com>
2022-11-17 22:59:13 -0800

    added all import and export functions, forgot to do mutliple acp

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
 /* Imports */
 
+import { getAccountInfo, getAnimalInvasion, getCars, getMovies } from './fetch-utils.js';
+import {
+    renderAccountInfo,
+    renderAnimalInvasions,
+    renderCars,
+    renderMovies,
+} from './render-utils.js';
+
 /* Get DOM Elements */
+const domainList = document.getElementById('domain-list');
 
 /* State */
 
 /* Events */
+window.addEventListener('load', async () => {
+    const animalInvaders = await getAnimalInvasion();
+    const accountInfo = await getAccountInfo();
+    const cars = await getCars();
+    const movies = await getMovies();
+    console.log(animalInvaders);
+
+    displayInvasions(animalInvaders);
+    displayAccountInfo(accountInfo);
+    displayCars(cars);
+    displayMovies(movies);
+});
 
 /* Display Functions */
+function displayInvasions(invaders) {
+    console.log(invaders);
+    for (let animalInvasion of invaders) {
+        const currentInvasion = renderAnimalInvasions(animalInvasion);
+        domainList.append(currentInvasion);
+    }
+}
 
-// (don't forget to call any display functions you want to run on page load!)
+function displayAccountInfo(accounts) {
+    for (let account of accounts) {
+        const currentAccount = renderAccountInfo(account);
+        domainList.append(currentAccount);
+    }
+}
+
+function displayCars(cars) {
+    for (let car of cars) {
+        const currentCar = renderCars(car);
+        domainList.append(currentCar);
+    }
+}
+
+function displayMovies(movies) {
+    for (let movie of movies) {
+        const currentMovie = renderMovies(movie);
+        domainList.append(currentMovie);
+    }
+}
